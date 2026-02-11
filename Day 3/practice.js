f646ee08ea0344f0951d33792dcc431e028920d1
Saurabh-Chede <saurabhchede21@gmail.com>
2026-02-11 20:14:10 +0530

    conditional statements

---
+++ Day 3/practice.js
@@ -0,0 +1,31 @@
+let fruit = 'apple'
+
+switch (fruit){
+    case 'apple':
+        console.log('apple')
+        break;
+    case 'banana':
+        console.log('banana')
+        break; 
+    case 'orange':
+        console.log('orange')
+        break;
+    default:
+        console.log('not found')  
+
+}
+
+let age = 18
+
+switch (age){
+    case '20':
+        console.log('20')
+        break;
+    case '14':
+        console.log('18')
+        break;
+    default:
+        console.log('not found')
+
+}
+
