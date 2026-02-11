a2f78161e9c71e68897b9034c0456e554124feb3
Saurabh-Chede <saurabhchede21@gmail.com>
2026-02-11 21:54:09 +0530

    conditional statements

--- Day 3/practice.js
+++ Day 3/practice.js
@@ -1,31 +1,67 @@
 let fruit = 'apple'
 
-switch (fruit){
+switch (fruit) {
     case 'apple':
         console.log('apple')
         break;
     case 'banana':
         console.log('banana')
-        break; 
+        break;
     case 'orange':
         console.log('orange')
-        break;
-    default:
-        console.log('not found')  
-
-}
-
-let age = 18
-
-switch (age){
-    case '20':
-        console.log('20')
-        break;
-    case '14':
-        console.log('18')
         break;
     default:
         console.log('not found')
 
 }
+
+// let age = 18
+
+// switch (age) {
+//     case '20':
+//         console.log('20')
+//         break;
+//     case '14':
+//         console.log('18')
+//         break;
+//     default:
+//         console.log('not found')
+
+// }
+
+let num = 10
+
+if (num > 0 || num <= 9) {
+    console.log('age is verified');
+}
+else console.log('not verified')
+
+
+function myName(){
+    console.log(`reurn`)
+    return myName
+}
+
+myName()
+
+// let db = {
+//     name : 'saurabh',
+//     age : 18,
+//     email: '@gmail.com'
+// }
+
+// const {name:userName, age:UserAGE} = db
+
+// console.log(UserAGE)
+
+let db = {
+  name: 'saurabh',
+  age: 18,
+  email: '@gmail.com'
+}
+
+const { name, age, email } = db;
+
+console.log(name, age, email);
+
 
